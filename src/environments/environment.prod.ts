b9f85cf4a0d918fe7f51923e4a69dfba1269473f
Bernardo Jurkic <bernardo.jurkic@reddia-x.com>
2026-01-23 12:45:06 +0100

    Changed auth endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://secret-earth-49998-b5c7e6fe39d1.herokuapp.com',
-  authServerUrl: 'https://secret-earth-49998-b5c7e6fe39d1.herokuapp.com',
+  apiBaseUrl: 'https://auth.reddia-x.com',
+  authServerUrl: 'https://auth.reddia-x.com',
   // URL to redirect after successful login
-  postLoginRedirectUrl: 'https://0a12ca40.rdx-auth.pages.dev'
+  postLoginRedirectUrl: 'https://login.reddia-x.com'
 };
